a481c1991603ab4990351d17b2db794c7716bd5f
Yogitaba Gohil <yogitabagohil1212@gmail.com>
2022-10-22 19:58:05 +0300

    About section added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Main from '../components/Main'
-import Navbar from '../components/Navbar'
+import Head from 'next/head';
+import About from '../components/About';
+import Main from '../components/Main';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <Main />
+      <About />
 
      
     </div>
